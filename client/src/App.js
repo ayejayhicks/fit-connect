72b0bf392efbddb21dc3ebfab2635b8f9c75612b
wildnei <wildneisuane@gmail.com>
2021-05-08 22:07:34 -0400

    fixing package.json issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD:src/App.js
-// import './App.css';
+
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Pages/Home';
@@ -22,32 +21,6 @@ class App extends Component {
       </React.Fragment>
     );
   }
-=======
-import logo from './logo.svg';
-import './App.css';
-import React from "react";
-
-function App() {
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
->>>>>>> main:client/src/App.js
 }
 
 export default App;
