dff856796b1a5a67ee5061727c7bebfe8b5531f3
wildnei <wildneisuane@gmail.com>
2021-05-09 20:06:04 -0400

    Adding two sections to main page

--- client/src/Components/NavigationBar.js
+++ client/src/Components/NavigationBar.js
@@ -1,27 +1,30 @@
 import React from 'react'
-import { Nav, Navbar, Button, Container } from 'react-bootstrap'
+import { Nav, Navbar, Button } from 'react-bootstrap'
 import navBarImage from '../Assets/fitness-navbar.svg'
+import "./NavigationBar.css"
 
 export const NavigationBar = () => (
-    <React.Fragment>
-        <Navbar bg="light" variant="liggitht">
-        <Container fluid>
-            <Navbar.Brand href="/">
-                <img
-                    alt=""
-                    src={navBarImage}
-                    width=""
-                    height=""
-                    className="d-inline-block align-top"
-                />{' '}
-    </Navbar.Brand>
-                <Nav className="ml-auto">
-                <Nav.Link href="#deets">About</Nav.Link>
-                <Nav.Link href="#memes">Features</Nav.Link>
-                <Nav.Link href="#deets">Login</Nav.Link>
-                <Button variant="outline-info">Search</Button>
-            </Nav>
-        </Container>
+
+    
+        <Navbar id="navBackground" collapseOnSelect expand="md">
+                <Navbar.Brand href="/">
+                    <img
+                        alt=""
+                        src={navBarImage}
+                        width=""
+                        height=""
+                        // className="d-inline-block align-top"
+                    />{' '}
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#deets">About</Nav.Link>
+                        <Nav.Link href="#memes">Features</Nav.Link>
+                        <Nav.Link href="#deets">Login</Nav.Link>
+                        <Button variant="outline-info">Search</Button>
+                    </Nav>
+                </Navbar.Collapse>
         </Navbar>
-        </React.Fragment>
+       
 );
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import { HeroCarousel } from '../Components/HeroCarousel'
 import { Layout } from '../Components/Layout';
 import { NavigationBar } from '../Components/NavigationBar';
-import { Cards } from '../Components/Cards'
+import { Cards } from '../Components/Cards';
+import { FirstSection } from '../Components/FirstSection';
+import { SecondSection } from '../Components/SecondSection';
 
 export const Home = () => {
     return (
         <React.Fragment>
+            <Layout>
             <NavigationBar />
+            </Layout>
                 <HeroCarousel />
             <Layout>
                 <Cards>
                 </Cards>
             </Layout>
+            <FirstSection>
+            </FirstSection>
+            <SecondSection>
+            </SecondSection>
         </React.Fragment>
     );
 }
