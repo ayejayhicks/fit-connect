e319f4701a372bea3c5981fbb99226a1a1b774a3
Deepali Garg <deepaligarg.chd@gmail.com>
2021-05-15 09:43:58 -0400

    added logout routes api

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const eventRoutes = require('./eventRoutes');
 const loginRoutes = require('./loginRoutes');
+const logoutRoutes = require('./logoutRoutes');
 
 router.use('/users', userRoutes);
 router.use('/events', eventRoutes);
 router.use('/login', loginRoutes);
+router.use('/logout', logoutRoutes);
 
 module.exports = router;
