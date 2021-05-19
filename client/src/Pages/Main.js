43c754c561c1340bcb89e2f06691e0e6a8fdb31e
wildnei <wildneisuane@gmail.com>
2021-05-19 14:15:26 -0400

    fixing main page

--- client/src/Pages/Main.js
+++ client/src/Pages/Main.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import { NavigationBar } from '../Components/Home_Components/NavigationBar';
+import { Layout } from '../Components/Home_Components/Layout';
+import { MainCards } from '../Components/Main_Components/MainCards'
+import { FooterSection } from '../Components/Home_Components/Footer';
+import { FooterContent } from '../Components/Home_Components/FooterContent';
 
 const Main = () => {
     return (
-        <div>
-            <h1>
-                Hello World
-            </h1>
-        </div>
-    )
+        <React.Fragment>
+            <Layout>
+                <NavigationBar />
+            </Layout>
+            
+            <Layout>
+                <MainCards>
+                </MainCards>
+            </Layout>
+
+            <FooterSection>
+                <Layout>
+                    <FooterContent>
+                    </FooterContent>
+                </Layout>
+            </FooterSection>
+        </React.Fragment>
+    );
 }
 
 export default Main;
