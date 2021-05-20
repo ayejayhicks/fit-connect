3ee5957bdee8e4984239542f2326588e952ee324
Courtney Fox <courtney.fox@me.com>
2021-05-19 23:37:41 -0400

    created POST to register user for event

--- routes/api/eventRoutes.js
+++ routes/api/eventRoutes.js
@@ -22,4 +22,26 @@ router.post("/", async (req, res) => {
   }
 });
 
+// POST to register user for event
+// POST: /api/events/:id/register
+router.post("/:id/register/:user_id", async ({ params: { id, user_id } }, res) => {
+  console.log('event id', id);
+  console.log('user id', user_id);
+  try {
+    const user = await db.User.findOne({ _id: user_id })
+    console.log('user', user);
+
+    const event = await db.Event.findOneAndUpdate({ _id: id }, { $addToSet: { users: user.id } }, { new: true })
+
+    user.events.push(event.id);
+    await user.save();
+
+    console.log('event', event);
+    res.json(event);
+  } catch(err) {
+      console.log('err', err);
+      res.json(err);
+  };
+});
+
 module.exports = router;
